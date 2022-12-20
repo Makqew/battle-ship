cb5a9912d3aa3d07c558d359edaef477ff03da67
Makqew <112494472+Makqew@users.noreply.github.com>
2022-12-20 23:03:05 +0600

    Initial implementation of gameBoard factory

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,6 @@
 console.log('hi');
-export const ship = (length, numOfHits, sunk) => {
+export const ship = (length, numOfHits, sunk, direction) => {
+    let shipData = []
     const hit = () => {
         numOfHits += 1
         isSunk()
@@ -12,10 +13,44 @@ export const ship = (length, numOfHits, sunk) => {
             console.log('Still alive!')
         }
     }
-    return {length, get numOfHits(){return numOfHits}, get sunk(){return sunk}, hit}
+    if(direction == 'vertical'){
+        for(let i = 0; i < length; i++){
+            shipData.push([1]);
+        }
+    } else {
+        for(let i = 0; i < length; i++){
+            shipData.push(1);
+        }
+    }
+    return {length, get numOfHits(){return numOfHits}, get sunk(){return sunk},get direction(){return direction},shipData, hit}
 }
-const black = ship(4,0,false)
+// const black = ship(4,0,false)
 // black.hit();
 // black.hit();
 
 // console.log(black)
+
+export const gameBoard = (shipPlayer) => {
+    let board = [['x', 'x', 'x', 'x'],['x', 'x', 'x', 'x'],['x', 'x', 'x', 'x'],['x', 'x', 'x', 'x']];
+    let tooBig = false;
+    const shipPlacement = [3,0];
+    if(shipPlayer.direction == 'vertical'){
+        for(let j = shipPlacement[0]; j < shipPlayer.length; j++){
+            if(j < board.length){
+                board[j][shipPlacement[1]] = 1;
+            }else {
+                tooBig = true;
+            }    
+        } 
+    }else {
+        for(let k = shipPlacement[1]; k < shipPlayer.length; k++){
+            board[shipPlacement[0]][k] = 1;    
+        }
+    }
+
+    const showBoard = () => {
+        console.log(board)
+    }
+
+    return {board, get tooBig(){return tooBig}}
+}
--- test/stack.test.js
+++ test/stack.test.js
@@ -1,13 +1,27 @@
-import {ship} from '../src/javascript/index'
+import {gameBoard, ship} from '../src/javascript/index'
 
 describe('Ship', () => {
-    const white = ship(14, 12, false)
+    const white = ship(4, 2, false, 'vertical')
     test('Hitting the ship', () => {
         white.hit()
-        expect(white.numOfHits).toBe(13)
+        expect(white.numOfHits).toBe(3)
     })
     test('Sunk or not', () => {
         white.hit()
         expect(white.sunk).toBe(true)
+    })
+    test('Placement', () => {
+        expect(white.shipData).toStrictEqual([[1],[1],[1],[1]])
+    })
+})
+
+describe('GameBoard', () => {
+    const black = ship(4,0,false, 'vertical');
+    const board1 = gameBoard(black);
+    // test('placement',() => {
+    //     expect(board1.board).toStrictEqual([[1, 'x', 'x', 'x'],[1, 'x', 'x', 'x'],[1, 'x', 'x', 'x'],[1, 'x', 'x', 'x']])
+    // })
+    test('inapropriate placing', () => {
+        expect(board1.tooBig).toBe(false)
     })
 })
