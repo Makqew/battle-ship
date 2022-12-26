5f4cb206836ff4802e010ec4b20eaeed914fba64
Makqew <112494472+Makqew@users.noreply.github.com>
2022-12-26 22:54:45 +0600

    Displaying the preview of the ship placement

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -23,5 +23,34 @@ import {player} from './player'
 
 // let nameOfPlayer = prompt('give a name');
 // alert(nameOfPlayer);
-// let ship1 = ship(3,0,false, 'vertical')
+let ship1 = ship(3,0,false, 'vertical')
 // let player1 = player(nameOfPlayer,ship1,1)
+
+let firstPlayerBoard = gameBoard(ship1, [3,0])
+let playerBoard = document.getElementById('player');
+let boardRow = playerBoard.getElementsByClassName('row');
+for(let row of boardRow){
+    let cells = row.getElementsByClassName('cell');
+    for(let elem of cells){
+        elem.addEventListener('mouseover', (event) => {
+            // event.currentTarget.style.backgroundColor = "red"
+            // console.log(Array.from(boardRow).indexOf(event.currentTarget.parentNode)) // position of row
+            // console.log(Array.from(cells).indexOf(event.currentTarget)) // position of column
+            let currentPosition = Array.from(cells).indexOf(event.currentTarget);
+            let cellsArr = Array.from(cells);
+            for(let i = 0; i < ship1.numOfCells; i++){
+                cellsArr[currentPosition].style.backgroundColor = "red";
+                currentPosition += 1;
+            }
+        })
+        elem.addEventListener('mouseout', (event) => {
+            let currentPosition = Array.from(cells).indexOf(event.currentTarget);
+            let cellsArr = Array.from(cells);
+            for(let i = 0; i < ship1.numOfCells; i++){
+                cellsArr[currentPosition].style.backgroundColor = "black";
+                currentPosition += 1;
+            }
+            
+        })
+    }
+}
